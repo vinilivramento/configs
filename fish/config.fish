4290f4030040821361b08b83eadadac294692a13
Vinicius <vinilivramento@gmail.com>
2024-01-28 17:23:57 +0000

    Adjusting folders

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 set -g theme_color_scheme zenburn
 
 set -U FZF_LEGACY_KEYBINDINGS 0
@@ -6,9 +10,9 @@ set -x EDITOR nvim
 set -gx RG_CMD 'rg --color=always --files -i --hidden --follow --smart-case -g "!{.git,node_modules}*" -g "!{*.o*}" -g "!{*.clangd*}" -g "!{*build*}/*" -g "!*cmake" '
 set -gx FZF_FIND_FILE_COMMAND $RG_CMD
 
-bind \cp 'vim $(fzf);'
-
 set -x GCC_COLORS 'error=01' //check rest
+
+bind \cp 'vim $(fzf);'
 
 alias grep rg
 alias del "mv -t /tmp"
